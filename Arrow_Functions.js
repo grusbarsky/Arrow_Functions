6c39dcaddfb81f02e957e1139f9151130db89190
Greta Rusbarsky <Greta@Gretas-MacBook-Pro.local>
2021-04-01 10:05:15 -0500

    first commit

---
+++ Arrow_Functions.js
@@ -0,0 +1,24 @@
+//ES5 Map Callback
+function double(arr) {
+    return arr.map(function(val) {
+      return val * 2;
+    });
+  }
+
+//ES2015 Arrow Function Callback
+const double = arr => arr.map(val => val * 2);
+
+// Refactor with Arrow Function
+function squareAndFindEvens(numbers){
+    var squares = numbers.map(function(num){
+      return num ** 2;
+    });
+    var evens = squares.filter(function(square){
+      return square % 2 === 0;
+    });
+    return evens;
+  }
+
+//New With Arrow Functions
+
+const squareAndFindEvens = numbers => numbers.map(num => num ** 2).filter(square => square % 2 === 0);
